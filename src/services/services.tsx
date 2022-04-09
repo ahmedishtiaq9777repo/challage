0b31d931963e9739ea029c4393f2be09f78dcae7
ahmedishtiaq9777 <50663545+ahmedishtiaq9777@users.noreply.github.com>
2022-04-09 06:47:38 +0500

    services file

--- src/services/services.tsx
+++ src/services/services.tsx
@@ -1,40 +1,39 @@
-import React from "react";
-import TasksData from "../db/dbStorage";
-import {useDispatch } from "react-redux";
+
+
 import { addTasktoList } from "../store/action";
-import { useToast } from "native-base";
-const  NavigationService=(navigation,dispatch,localdb,toast)=>{
+
+const NavigationService = (navigation, dispatch, localdb, toast) => {
 
 
     const unsubscribe = navigation.addListener("focus", () => {
-   
+
 
         try {
-          localdb.createTaskTable().then((res) => {
-  
-  
-            localdb
-              .getalltasks()
-              .then((records) => {
-  
-                dispatch(addTasktoList(records));
-              })
-              .catch((error) => {
+            localdb.createTaskTable().then((res) => {
+
+
+                localdb
+                    .getalltasks()
+                    .then((records) => {
+
+                        dispatch(addTasktoList(records));
+                    })
+                    .catch((error) => {
+                        toast.show({ title: error });
+                    });
+
+
+
+            }).catch((error) => {
                 toast.show({ title: error });
-              });
-  
-  
-  
-          }).catch((error) => {
-            toast.show({ title: error });
-          });
-  
+            });
+
         } catch (error) {
-          toast.show({ title: error });
+            toast.show({ title: error });
         }
-  
-      });
-      return unsubscribe;
+
+    });
+    return unsubscribe;
 
 
 };
